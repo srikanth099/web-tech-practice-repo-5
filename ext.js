1d36d1871d02c1be4715e49f4589b36c3c8af51c
SRIKANTH GUMMA <143108748+srikanth099@users.noreply.github.com>
2024-10-20 20:50:52 +0530

    Add files via upload
    This repository contains various projects and exercises to practice HTML and CSS. It includes foundational topics such as semantic HTML tags, responsive design using Flexbox and Grid, CSS animations, and more. Ideal for mastering the basics of front-end web development.

---
+++ ext.js
@@ -0,0 +1,9 @@
+console.log("This Is External File")
+console.error("This Is External File")
+console.warn("This Is External File")
+// function main()
+// {
+// 	console.log("main starts");
+// 	console.log("main ends");
+// }
+// main();
